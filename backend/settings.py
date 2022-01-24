ac0a84943275a8a1dd6659a258295d273f9f9997
Naveen <78008387+Naveen2004@users.noreply.github.com>
2022-01-24 18:09:37 +0530

    production build setting #3 Modified settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -78,6 +78,9 @@ CORS_ALLOW_CREDENTIALS = True
 # CSRF SETUP
 CSRF_TRUSTED_ORIGINS = CORS_ALLOWED_ORIGINS
 CSRF_COOKIE_SAMESITE = "None"
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SAMESITE = "None"
+SESSION_COOKIE_SECURE = True
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
